2682525136c9f2b72b9aff8860bbb7f45e639881
adfaure <adrien.faure2@gmail.com>
2017-11-04 17:41:12 +0100

    add player movements and viewport collisions

--- minigames/death_invaders/scenes/game.gd
+++ minigames/death_invaders/scenes/game.gd
@@ -13,11 +13,27 @@ func _ready():
 
 func _fixed_process(delta):
 	
-	# if player has shoot
+	# Move the player
+	var player = get_node("player")
+	if Input.is_key_pressed(KEY_UP) : # move up
+		player.move(Vector2(0, 100 * -delta))
+		# if out of screen then replace player
+		if player.get_pos().y - player.get_item_rect().size.height/2 < 0 : 
+			player.set_pos(Vector2(player.get_pos().x, player.get_item_rect().size.height/2)) 
+	
+	elif Input.is_key_pressed(KEY_DOWN) : # move down
+		player.move(Vector2(0, 100 * delta))
+		# if out of screen then replace player
+		if player.get_pos().y + player.get_item_rect().size.height/2 > self.get_viewport_rect().size.height : 
+			player.set_pos(Vector2(player.get_pos().x, self.get_viewport_rect().size.height - player.get_item_rect().size.height/2)) 
+	
+	
+	
+	# if player has shoot then move the bullet and check collisions
 	if has_node("bullet") :
 		# move the bullet
 		var bullet = get_node("bullet")
-		bullet.move(Vector2(100 * delta, 0))
+		bullet.move(Vector2(200 * delta, 0))
 		var b_pos = bullet.get_pos()
 		
 		#check if bullet collides a dead or the and of screen
@@ -25,4 +41,6 @@ func _fixed_process(delta):
 			if bullet.is_colliding() : # if collides a dead, rekill the dead
 				bullet.get_collider().queue_free()
 			bullet.queue_free()
+			
+	
 	
