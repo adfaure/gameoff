06624edc70fae3100d203740b09a888a0cf4e51d
A. Richard <antoine.richard0@dauphine.eu>
2017-11-07 21:45:10 +0100

    game rect

--- minigames/death_invaders/scripts/game_gui.gd
+++ minigames/death_invaders/scripts/game_gui.gd
@@ -3,8 +3,25 @@ extends Node2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-
-
+func _draw():
+	#draw_line(Vector2(25, 130), Vector2(625, 130), Color(255, 255, 255), 5)
+	
+	var v_pos = get_node("Control").get_pos()
+	var v_rect_size = get_node("Control/Viewport").get_rect().size
+	
+	
+	draw_line(Vector2(v_pos.x - 2.5, v_pos.y), 
+	Vector2(v_pos.x + v_rect_size.width, v_pos.y), 
+	Color(255, 255, 255), 5)
+	draw_line(Vector2(v_pos.x + v_rect_size.width, v_pos.y - 2.5), 
+	Vector2(v_pos.x + v_rect_size.width, v_pos.y + v_rect_size.height), 
+	Color(255, 255, 255), 5)
+	draw_line(Vector2(v_pos.x + v_rect_size.width + 2.5, v_pos.y + v_rect_size.height),
+	Vector2(v_pos.x, v_pos.y + v_rect_size.height), 
+	Color(255, 255, 255), 5)
+	draw_line(Vector2(v_pos.x, v_pos.y + v_rect_size.height + 2.5), 
+	Vector2(v_pos.x, v_pos.y), 
+	Color(255, 255, 255), 5)
+	
 func _ready():
-	draw_line(Vector2(0, 0), Vector2(50, 50), Color(255, 0, 0), 5)
-	update()
+	pass
