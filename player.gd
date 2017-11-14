d000c61ae4c7a02540874b2d8b3bf1bb01e5558a
Meinew <moi.meinew@gmail.com>
2017-11-14 20:41:51 +0100

    Fixed some bugs with moves

--- player.gd
+++ player.gd
@@ -7,7 +7,8 @@ export var RUN_SPEED = 30
 export var WALK_SPEED = 10
 export var BASE_SPEED = 10
 
-var run = false
+
+var isFlipped = false
 
 func _ready():
     set_process_input(true);
@@ -16,6 +17,7 @@ func _ready():
 
 func _fixed_process(delta):
 	var boost = BASE_SPEED
+	var run = false
 	
 	var input_dir = Vector2(0, 0);
 	if Input.is_action_pressed("ui_right"): input_dir += Vector2(1,0);
@@ -23,7 +25,7 @@ func _fixed_process(delta):
 	if Input.is_action_pressed("ui_run"): run = true
 	
 	var animation
-	if (input_dir.x > 0) :
+	if (input_dir.x != 0) :
 		if run :
 			animation = "run"
 			boost *= RUN_SPEED
@@ -32,13 +34,21 @@ func _fixed_process(delta):
 			boost *= WALK_SPEED
 	else :
 		animation = "idle";
-		
-	set_pos(get_pos() + input_dir * boost * delta)
+
+	var nextPos = get_pos() + input_dir * boost * delta
+	nextPos.x = max(0, nextPos.x)
+	var maxX = get_parent().find_node("floor").get_region_rect().size.x
+	nextPos.x = min(maxX, nextPos.x)
+	set_pos(nextPos)
 	
 	var anim = find_node("Animation")
 	var current = anim.get_current_animation()
 	if current != animation:
 		anim.play(animation);
-		
+	
+	var scale=get_scale()
+	if (input_dir.x < 0 && !isFlipped) ||  (input_dir.x > 0 && isFlipped) :
+		set_scale(Vector2(-scale.x,scale.y))
+		isFlipped = not isFlipped
 	
 	
